8f58c1b92fe1a1babd88f5da2e40275d23aa9f19
goldenclouds17 <fernandosand1173@gmail.com>
2025-07-06 18:23:55 -0600

    Add files via upload

---
+++ inventario.js
@@ -0,0 +1,124 @@
+export const vapeModels = [
+  {
+  id: 'model1',
+  name: 'Lost Vape 7500 hits',
+  description: 'Vape potente con gran variedad de sabores, hasta 7500 caladas.',
+  image: 'https://i.postimg.cc/cvW2vyGf/IMG-6015.webp',
+  flavors: [
+    { id: 'f101', name: 'Melon Mint 🍉', price: 210 },
+    { id: 'f102', name: 'Green Apple Pop 🍏', price: 210 },
+    { id: 'f103', name: 'Rainbow Drop 🌈🍬', price: 210 },
+    { id: 'f104', name: 'Colombian Coffee 🇨🇴☕️', price: 210 },
+    { id: 'f105', name: 'Blackcurrant Mixed Berries 🖤', price: 210 },
+    { id: 'f106', name: 'Green Storm 💚🌩️', price: 210 },
+    { id: 'f107', name: 'BlackDragon Ice ❤️🫐', price: 210 },
+    { id: 'f108', name: 'Bama Slammer 🍉🌿🌿', price: 210 },
+    { id: 'f109', name: 'Blueberry Rose Mint 🫐🌹', price: 210 },
+    { id: 'f110', name: 'Cherry Berry 🍒🍇', price: 210 },
+    { id: 'f111', name: 'Orange Ice 🍊🧡🧊', price: 210 },
+    { id: 'f112', name: 'Strawberry Raspberry Cherry 🍓🍒', price: 210 },
+    { id: 'f113', name: 'Lush Ice 🍉🧊', price: 210 },
+    { id: 'f114', name: 'Blueberry Pie 🫐🍰', price: 210 },
+    { id: 'f115', name: 'Pineapple Lemonade 🍍💛', price: 210 },
+    { id: 'f116', name: 'Café Mocha ☕️🍫', price: 210 },
+    { id: 'f117', name: 'Euphoria Pudding 😵‍💫🌽🍮', price: 210 },
+    { id: 'f118', name: 'Peachy Mix 🍑💗', price: 210 }
+  ]
+},
+
+  {
+  id: 'model2',
+  name: 'Tyson Vape 7500 hits',
+  description: 'Vape de alto rendimiento con sabores frutales intensos. Hasta 7500 caladas.',
+  image: 'https://i.postimg.cc/LngtRWyw/IMG-6018.jpg',
+  flavors: [
+    { id: 'f201', name: 'Frozen Banana 🍌', price: 245 },
+    { id: 'f202', name: 'Apple Gummies 🍏', price: 245 },
+    { id: 'f203', name: 'Pineapple Kiwiberry 🍍', price: 245 },
+    { id: 'f204', name: 'Mango Lychee 🥭', price: 245 }
+  ]
+},
+
+    {
+  id: 'model3',
+  name: 'PacksPod 5000 hits',
+  description: 'Dispositivo con 5000 inhalaciones para una experiencia duradera.',
+  image: 'https://i.postimg.cc/CZzN1m5j/IMG-8195.jpg',
+  flavors: [
+    { id: 'f7', name: 'Cucumber Menthol 🥒', price: 265 },
+    { id: 'f8', name: 'Sweet Cloud ☁️', price: 265 },
+    { id: 'f9', name: 'Spark (Energy juice) 🐂', price: 265 },
+    { id: 'f10', name: 'Frozen Snow Cone 🍧🌈', price: 265 },
+    { id: 'f11', name: 'Miami haze 🏝️🍉', price: 265 }
+  ]
+},
+
+  {
+  id: 'model4',
+  name: 'PrivBar 15k hits',
+  description: 'Dispositivo con 15,000 inhalaciones para uso prolongado.',
+  image: 'https://i.postimg.cc/184Cn9Yn/IMG-6019.jpg',
+  flavors: [
+    { id: 'f12', name: 'Cherry Paradise 🍒', price: 285 },
+    { id: 'f13', name: 'Florida Lemonade 🍋', price: 285 },
+    { id: 'f14', name: 'Peach Berry Ice 🍑🧊', price: 285 },
+    { id: 'f15', name: 'Strawberry Mint Candy 🍓🍬', price: 285 },
+    { id: 'f16', name: 'Triple Mango 🥭 💛', price: 285 },
+    { id: 'f17', name: 'Watermelon Chill 🍉', price: 285 },
+    { id: 'f18', name: 'Kiwi Straw Bubblegum 🥝🍓💗', price: 285 }
+  ]
+},
+
+  {
+  id: 'model5',
+  name: 'Waka 15 MIL HITS',
+  description: 'Dispositivo con 15,000 inhalaciones para uso prolongado y sabores únicos.',
+  image: 'https://i.postimg.cc/QVsfggyr/IMG-1324.png',
+  flavors: [
+    { id: 'f19', name: 'Watermelon Chill 🍉', price: 365 },
+    { id: 'f20', name: 'Jamaica ❤️', price: 365 },
+    { id: 'f21', name: 'Strawberry Banana 🍓🍌', price: 365 },
+    { id: 'f22', name: 'Strawberry Burst 🍓❤️', price: 365 },
+    { id: 'f23', name: 'Cherry Berry 🍒', price: 365 },
+    { id: 'f24', name: 'Peach Mango Watermelon 🍑🥭🍉', price: 365 }
+  ]
+},
+
+  {
+  id: 'model6',
+  name: 'KUZ 25,000 hits',
+  description: 'Dispositivo con 25,000 inhalaciones para los usuarios más exigentes.',
+  image: 'https://i.postimg.cc/CZnty734/IMG-6021.jpg',
+  flavors: [
+    { id: 'f25', name: 'Strawberry Watermelon 🍓🍉', price: 375 },
+    { id: 'f26', name: 'Súper Berry ❤️⚡️', price: 375 },
+    { id: 'f27', name: 'Mango Sluhie 🥭🧡', price: 375 },
+    { id: 'f28', name: 'Sex ok the beach 🔥🏝️', price: 375 },
+    { id: 'f29', name: 'Blue Razz Sluhie 🫐', price: 375 },
+    { id: 'f30', name: 'Miami Mint 🌿🏝️🧊', price: 375 }
+  ]
+},
+
+  {
+    id: 'model7',
+    name: 'Vape Mini',
+    description: 'Pequeño pero poderoso, ideal para llevar en el bolsillo.',
+    image: 'https://via.placeholder.com/150/8A2BE2/FFFFFF?text=Vape+Mini',
+    flavors: [
+      { id: 'f19', name: 'Arándano Azul', price: 180 },
+      { id: 'f20', name: 'Cereza Ácida', price: 180 },
+      { id: 'f21', name: 'Mora Silvestre', price: 180 },
+    ]
+  },
+  {
+    id: 'model8',
+    name: 'Vape Pro',
+    description: 'Para los conocedores, control total y personalización.',
+    image: 'https://via.placeholder.com/150/A52A2A/FFFFFF?text=Vape+Pro',
+    flavols: [
+      { id: 'f22', name: 'Café Caramelo', price: 500 },
+      { id: 'f23', name: 'Crema Irlandesa', price: 500 },
+      { id: 'f24', name: 'Chocolate Oscuro', price: 500 },
+    ]
+  },
+];
